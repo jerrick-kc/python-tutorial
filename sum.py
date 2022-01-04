b6e5e6e9f117aba1c8ae3cd01059d46e60ee7d19
xjerrickx <96703726+jerrick-kc@users.noreply.github.com>
2022-01-04 02:29:25 +0000

    table

---
+++ sum.py
@@ -0,0 +1,24 @@
+total = 0
+answer = 1
+
+numbers = range(1, 20)
+print (numbers)
+
+for number in numbers:
+  total = total + number
+
+print (total)
+
+for number in numbers:
+  answer = answer * number
+
+print (answer)
+  
+sum = 0
+values = range(20)
+
+for value in values:
+  if value%2 == 0:
+    sum = sum + value
+
+print(sum)
